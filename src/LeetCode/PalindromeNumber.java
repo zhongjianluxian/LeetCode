96c7201c6dfa448f5efc40682a9179005267d840
Chen Xie <chenxie.dev@gmail.com>
2012-12-23 19:32:56 -0700

    First version of Plindrome number

---
+++ src/LeetCode/PalindromeNumber.java
@@ -0,0 +1,27 @@
+package LeetCode;
+
+public class PalindromeNumber {
+    boolean isPalindrome(int x) 
+    {
+        if (x < 0) 
+            return false;
+        int div = 1;
+        while (x / div >= 10)
+            div *= 10;
+        
+        while(div>1)
+        {
+            int l = x/div;
+            int r = x%10;
+            if(l!=r)
+                return false;
+            //eliminate the first and last digits
+            //subtle while the second digit is '0'
+            x = (x%div)/10;
+            div /= 100;
+        }
+
+        return true;
+    }
+}
+
