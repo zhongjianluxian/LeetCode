f58d59cd5c72446b3c1dd29d28bbbafd33186fc5
Chen Xie <chenxie.dev@gmail.com>
2013-01-05 23:49:56 -0700

    Second version of the unique paths problem.
    DP solution: maintain an 1xm array to keep intermediate results

--- src/LeetCode/UniquePaths.java
+++ src/LeetCode/UniquePaths.java
@@ -7,10 +7,11 @@ package LeetCode;
 
 public class UniquePaths {
     public int uniquePaths(int m, int n) {
-       return uniquePaths(0,0,m,n);
+       //return uniquePathsR(0,0,m,n);
+       return uniquePathsDP(m,n);
     }
-    //calculate the unique paths from (i,j) to (m,n)
-    private int uniquePaths(int i, int j, int m, int n)
+    //Solution 1: calculate the unique paths from (i,j) to (m,n), in a recursive way
+    private int uniquePathsR(int i, int j, int m, int n)
     {
         if(m*n == 0)
             return 0;
@@ -18,6 +19,24 @@ public class UniquePaths {
             return 1;        
         if(i==m-1 || j==n-1)
             return 1;
-        return uniquePaths(i+1,j,m,n) + uniquePaths(i,j+1,m,n);
+        return uniquePathsR(i+1,j,m,n) + uniquePathsR(i,j+1,m,n);
+    }
+    //Solution 2: dynamic programming 
+    private int uniquePathsDP(int m, int n)
+    {
+        if(m*n == 0)
+            return 0;
+        if(m == 1 || n== 1)
+            return 1;
+        int[] values = new int[m];
+        for(int i=0;i<m;i++)
+            values[i] = 1;
+        for(int i=n-2;i>=0;i--)
+            for(int j=m-2;j>=0;j--)
+            {
+                values[j] = values[j+1] + values[j];
+            }
+        
+        return values[0];
     }
 }
