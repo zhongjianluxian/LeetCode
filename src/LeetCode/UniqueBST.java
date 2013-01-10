1d547bb9174bab35407fd91e5e2b18f459a5baf6
Chen Xie <chenxie.dev@gmail.com>
2013-01-09 22:24:27 -0700

    First version of the unique path problem.
    DP/Memorization solution. The time complexity and space complexity are all O(n).

---
+++ src/LeetCode/UniqueBST.java
@@ -0,0 +1,32 @@
+/*
+ * Given n, how many structurally unique BST's (binary search trees) that store values 1...n?
+ */
+package LeetCode;
+
+public class UniqueBST {
+    public int numTrees(int n) {
+        int[] numbers = new int[n+1];
+        for(int i=0;i<n+1;i++)
+        {
+            if(i==0)
+                numbers[i] = 1;
+            else if(i==1)
+                numbers[i] = 1;
+            else if(i==2)
+                numbers[i] = 2;
+            else
+                numbers[i] = this.calcNumbers(i, numbers);
+        }
+        return numbers[n];
+    }
+    private int calcNumbers(int i, int[] numbers)
+    {
+        int sum=0, item;
+        for(int j=0;j<i;j++)
+        {
+            item = numbers[j]*numbers[i-j-1];
+            sum+=item;
+        }
+        return sum;
+    }
+}
