0691ab090ef6a4543dcfaa469ff45201f0f9b4ea
Chen Xie <chenxie.dev@gmail.com>
2013-01-15 23:53:30 -0700

    First version of the merge two sorted list problem. Intently a not-in-place implementation.

--- src/LeetCode/LeetCode.java
+++ src/LeetCode/LeetCode.java
@@ -4,6 +4,8 @@
  */
 package LeetCode;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author chen
@@ -14,6 +16,19 @@ public class LeetCode {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //PalindromeNumber pn = new PalindromeNumber();
+        //System.out.println(pn.isPalindrome(1010101));
+        /*WordSearch ws = new WordSearch();
+        char[][] board = {
+            {'A','B','C','E'},
+            {'S','F','C','S'},
+            {'A','D','E','E'}*/
+        int[][] board  = {{0,0,0,0,0},
+                          {0,0,0,0,1},
+                          {0,0,0,1,0},
+                          {0,0,1,0,0}
+                        };
+        
     }
-}
+    
+ }
